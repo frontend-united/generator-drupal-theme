88b67aa858e7ce9c3fc23483f867ef8d3695ec1e
Ian Carrico <git@iancarrico.com>
2014-11-14 18:24:20 -0600

    Fix label on Zen test

--- test/05-test-zen.js
+++ test/05-test-zen.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('Aurora theme (Corona distribution)', function () {
+describe('Zen Sub-theme', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
